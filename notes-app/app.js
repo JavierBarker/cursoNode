6f0f7b849250fee7073551bd986e63bb5e1d52e7
JavierBarker <hernandez.javier2003@gmail.com>
2022-05-27 16:52:22 -0600

    change: notes and use yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -15,5 +15,53 @@ const sum = add(4, -2);
 console.log(sum);*/
 
 const getNotes = require('./notes.js');
+const validator = require('validator');
+const chalk = require('chalk');
+const yargs = require('yargs');
 
 console.log(getNotes());
+
+console.log(validator.isURL('http://email.com'));
+
+/*console.log(
+  chalk.green(
+    'I am a green line ' +
+      chalk.blue.underline.bold('with a blue substring') +
+      ' that becomes green again!'
+  )
+);*/
+// con la bandera -g es para indicar que el modulo se va a instalar de manera global
+
+yargs.command({
+  command: 'add',
+  describe: 'Add note',
+  handler: function () {
+    console.log('adding a new note');
+  },
+});
+
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+    console.log('removing note');
+  },
+});
+
+yargs.command({
+  command: 'list',
+  describe: 'List Notes',
+  handler: function () {
+    console.log('list notes');
+  },
+});
+
+yargs.command({
+  command: 'read',
+  describe: 'read note',
+  handler: function () {
+    console.log('reading note');
+  },
+});
+//para ver los comandos se tiene usa el 'node app.js --help'
+console.log(yargs.argv);
